2c24273ab8ab9fc099d67a85e3580b86290e9e73
Santiago Coelho <sc_12340@hotmail.com>
2024-03-20 02:11:14 -0300

    borrado de comentario

--- lib/BobDylan.rb
+++ lib/BobDylan.rb
@@ -13,14 +13,12 @@ spotify_api.get_album_cover('Bob Dylan - 1974 Planet Waves')
 hash_album = {}
 lines.each do |line|
     data = line.split(' ',2)
-    #cover = spotify_api.get_album_cover("Bob Dylan - #{data[0]} #{data[1]}")
-    cover = '1'
+    cover = spotify_api.get_album_cover("Bob Dylan - #{data[0]} #{data[1]}")
     album = Album.new(data[0], data[1], cover)
     hash_album[album.decade] ||= []
     hash_album[album.decade] << album
 end
 
 hash_album.each do |clave, valor|
-    puts clave
     valor.sort!
 end
